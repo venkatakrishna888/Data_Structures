87af0ffdad14438a936a01d491d23bfcfec401ef
VENKATAKRISHNA BATTULA <venkatakrisha42@gmail.com>
2026-03-24 08:29:12 +0530

    Create bubble_sort.py
    added succussfully

---
+++ bubble_sort.py
@@ -0,0 +1,10 @@
+arr = [5, 2, 9, 1, 7]
+
+n = len(arr)
+
+for i in range(n):
+    for j in range(0, n-i-1):
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+
+print("Sorted array:", arr)
